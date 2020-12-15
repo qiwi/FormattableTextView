298f1aa053e00a51bea970b1145c207a8c51fd40
Alexander Desyatov <a.desyatov@qiwi.com>
2020-12-15 11:01:17 +0300

    Add swift pm

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "FormattableTextView",
+    platforms: [
+            .iOS(.v8),
+        ],
+    products: [
+        // The external product of our package is an importable
+        // library that has the same name as the package itself:
+        .library(
+            name: "FormattableTextView",
+            targets: ["FormattableTextView"]
+        )
+    ],
+    targets: [
+        // Our package contains two targets, one for our library
+        // code, and one for our tests:
+        .target(name: "FormattableTextView", path: "FormattableTextView")
+    ]
+)
