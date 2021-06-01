1e248412a324a52fe6d7a985dd715ce177b1591e
Motyzhenkov Mikhail <m.motyzhenkov@qiwi.com>
2021-06-01 14:19:18 +0300

    fix a bug in WHOLE style appearance

--- FormattableTextView/FormattableInput.swift
+++ FormattableTextView/FormattableInput.swift
@@ -179,6 +179,7 @@ extension FormattableInputInternal {
 	
 	func setupFormatChars() {
 		formatInputChars = Set(formatSymbols.keys)
+		processNonInputSymbolsAtTheEnd()
 	}
 	
 	func setLeftInset() {
